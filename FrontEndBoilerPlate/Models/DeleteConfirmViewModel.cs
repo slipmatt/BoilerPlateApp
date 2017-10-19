2097c1fac0138af3edc54f07785302bd25a8bf5f
Imtiaz Buckas <imtiaz.pocketpc@gmail.com>
2017-10-19 20:54:42 +0200

    Added Generic Delete Modal

---
+++ FrontEndBoilerPlate/Models/DeleteConfirmViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace FrontEndBoilerPlate.Models
+{
+    [DataContract]
+    public class DeleteConfirmViewModel
+    {
+        [DataMember]
+        public string DeleteAction { get; set; }
+        [DataMember]
+        public string DeleteController { get; set; }
+        [DataMember]
+        public string HeaderText { get; set; }
+        [DataMember]
+        public int DeleteEntityId { get; set; }
+    }
+}
